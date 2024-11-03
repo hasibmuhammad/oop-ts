48284e43a06d38446293f8163ed0fe9d24094851
hasibmuhammad <hasibmu99@gmail.com>
2024-11-03 21:52:51 +0600

    practiced more oop concepts like type guard

--- module3/oop-javascript/src/animal.ts
+++ module3/oop-javascript/src/animal.ts
@@ -1,17 +1,47 @@
 class Animal {
-  constructor(
-    public name: string,
-    public species: string,
-    public sound: string
-  ) {}
+  constructor(public name: string, public species: string) {}
 
   makeSound() {
-    console.log(`The ${this.species} says ${this.sound}`);
+    console.log(`I am making sound.`);
   }
 }
 
-const dog = new Animal("Picku", "dog", "Ghew Ghew");
-const cat = new Animal("Chiku", "Cat", "Meaw Meaw");
+class Dog extends Animal {
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
 
-dog.makeSound();
-cat.makeSound();
+  makeBark() {
+    console.log("Dog is barking!");
+  }
+}
+
+class Cat extends Animal {
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
+
+  makeMeaw() {
+    console.log("Cat is meawing!");
+  }
+}
+
+// creating instance
+const dog = new Dog("Dog", "DOG");
+const cat = new Cat("Cat", "CAT");
+
+const isDog = (animal: Animal): animal is Dog => animal instanceof Dog;
+const isCat = (animal: Animal): animal is Cat => animal instanceof Dog;
+
+const getAnimal = (animal: Animal) => {
+  if (isDog(animal)) {
+    animal.makeBark();
+  } else if (isCat(animal)) {
+    animal.makeMeaw();
+  } else {
+    animal.makeSound();
+  }
+};
+
+getAnimal(dog);
+getAnimal(cat);
